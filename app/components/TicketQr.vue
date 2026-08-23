d195e6e02ff0bd17c26de2cc3f1afde52a22302e
Jackson Yarger <jacksonkyarger@gmail.com>
2026-08-23 16:51:28 -0700

    feat: add TicketQr component with optional logo overlay

---
+++ app/components/TicketQr.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import QrcodeVue from 'qrcode.vue'
+
+const props = withDefaults(defineProps<{
+  value: string
+  size?: number
+}>(), {
+  size: 220,
+})
+</script>
+
+<template>
+  <div class="relative inline-block" :style="{ width: `${props.size}px`, height: `${props.size}px` }">
+    <QrcodeVue :value="props.value" :size="props.size" level="M" render-as="svg" />
+    <div v-if="$slots.logo" class="absolute inset-0 flex items-center justify-center">
+      <slot name="logo" />
+    </div>
+  </div>
+</template>
